ab5286acab77073553ea688880ab7f8ef2c2b381
anthonypark <anthonypark6904@gmail.com>
2024-08-05 15:51:45 +0900

    unity env 작성 및 추가.
    TODO: vector observation을 config에서 추가해야함.

--- sheeprl/envs/unity_env.py
+++ sheeprl/envs/unity_env.py
@@ -1,66 +1,45 @@
-from __future__ import annotations
+import os
 
-from sheeprl.utils.imports import _IS_CRAFTER_AVAILABLE
-
-if not _IS_CRAFTER_AVAILABLE:
-    raise ModuleNotFoundError(_IS_CRAFTER_AVAILABLE)
-
-from typing import Any, Dict, List, Optional, Sequence, SupportsFloat, Tuple, Union
-
-import crafter
-import gymnasium as gym
+from typing import Any, Dict, Optional, SupportsFloat, Tuple, Union
 import numpy as np
+import gymnasium as gym
 from gymnasium import spaces
-from gymnasium.core import RenderFrame
+
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
 
 
-class CrafterWrapper(gym.Wrapper):
-    def __init__(self, id: str, screen_size: Sequence[int, int] | int, seed: int | None = None) -> None:
-        assert id in {"crafter_reward", "crafter_nonreward"}
-        if isinstance(screen_size, int):
-            screen_size = (screen_size,) * 2
-
-        env = crafter.Env(size=screen_size, seed=seed, reward=(id == "crafter_reward"))
+class UnityWrapper(UnityToGymWrapper):
+    def __init__(self, file_name, env_num_id) -> None:
+        print('initailizing!')
+        os.environ['DISPLAY'] = ':1'
+        print('env_num_id:', env_num_id)
+        print('base_port:',5005+env_num_id)
+        print('file name:',file_name)
+        env = UnityEnvironment(file_name, worker_id=env_num_id)
         super().__init__(env)
-        self.observation_space = spaces.Dict(
-            {
-                "rgb": spaces.Box(
-                    self.env.observation_space.low,
-                    self.env.observation_space.high,
-                    self.env.observation_space.shape,
-                    self.env.observation_space.dtype,
-                )
-            }
-        )
-        self.action_space = spaces.Discrete(self.env.action_space.n)
-        self.reward_range = self.env.reward_range or (-np.inf, np.inf)
-        self.observation_space.seed(seed)
-        self.action_space.seed(seed)
-        # render
-        self._render_mode: str = "rgb_array"
-        # metadata
-        self._metadata = {"render_fps": 30}
 
+    # render_mode는 무조건 rgb이지만,
     @property
-    def render_mode(self) -> str | None:
-        return self._render_mode
+    def render_mode(self):
+        return 'rgb_array'
 
-    def _convert_obs(self, obs: np.ndarray) -> Dict[str, np.ndarray]:
+    def _convert_obs(self, obs: Union[np.ndarray, Dict[str, np.ndarray]]) -> Dict[str, np.ndarray]:
         return {"rgb": obs}
 
+
     def step(self, action: Any) -> Tuple[Any, SupportsFloat, bool, bool, Dict[str, Any]]:
-        obs, reward, done, info = self.env.step(action)
-        return self._convert_obs(obs), reward, done and info["discount"] == 0, done and info["discount"] != 0, info
+        obs, reward, done, info = super().step(action)
+        if obs.dtype == np.float32:
+            obs = (obs * 255).astype(np.uint8)
+        return obs, reward, done, False, info
 
     def reset(
         self, *, seed: Optional[int] = None, options: Optional[Dict[str, Any]] = None
     ) -> Tuple[Any, Dict[str, Any]]:
-        self.env._seed = seed
-        obs = self.env.reset()
-        return self._convert_obs(obs), {}
+        obs = super().reset()
+        if obs.dtype == np.float32:
+            obs = (obs * 255).astype(np.uint8)
+        print('-------------- reset complete! ---------------')
+        return obs, {}
 
-    def render(self) -> Optional[Union[RenderFrame, List[RenderFrame]]]:
-        return self.env.render()
-
-    def close(self) -> None:
-        return
